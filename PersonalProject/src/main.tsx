46da0a0c09775f0ba2636ace4a2eb774fcdc3147
LAPTOP-RN4FBF9J\lhowa <lhowat624@hotmail.com>
2025-01-18 16:23:35 +0000

    commit

--- PersonalProject/src/main.tsx
+++ PersonalProject/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './project-ui/index.css'
 import App from './project-ui/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
