63cf29f9b776d8b3b462d3d585ffc01da73bce08
LAPTOP-RN4FBF9J\lhowa <lhowat624@hotmail.com>
2025-02-04 19:25:11 +0000

    Creating basic classes

--- PersonalProject/src/project-ui/pages/LoginPage.tsx
+++ PersonalProject/src/project-ui/pages/LoginPage.tsx
@@ -2,13 +2,13 @@ import { LoginButton } from '..'
 
 
 function LoginPage() {
-
   return (
-    <>
+    <div className='page-center'>
       <h1>Login</h1>
       <LoginButton />
-    </>
+    </div>
   )
 }
 
 export default LoginPage
+
