659330a74e5b44b88afeaaeab8fc875648f99e8d
LAPTOP-RN4FBF9J\lhowa <lhowat624@hotmail.com>
2025-02-08 11:32:40 +0000

    Fixing styling issues

--- PersonalProject/src/project-ui/auth/Login.tsx
+++ PersonalProject/src/project-ui/auth/Login.tsx
@@ -1,10 +1,11 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "rsuite";
 
 function LoginButton() {
   const { loginWithRedirect } = useAuth0();
 
   return (
-    <button
+    <Button
       onClick={() =>
           loginWithRedirect({
               appState: { returnTo: "/home" },
@@ -12,7 +13,7 @@ function LoginButton() {
       }
     >
       Log In
-    </button>
+    </Button>
   );
 };
 
