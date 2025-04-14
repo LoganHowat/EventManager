89ad11d24cb86e429b9e07c8d9bf178250de1ed7
LAPTOP-RN4FBF9J\lhowa <lhowat624@hotmail.com>
2025-04-14 19:42:57 +0100

    Removing API so I can change to Supabase

--- PersonalProject/src/project-api/models/User.py
+++
@@ -1,9 +0,0 @@
-from database import db
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
--- PersonalProject/src/project-api/routes/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from .user_routes import user_routes
-from .product_routes import product_routes
-
-# Create a single Blueprint
-routes_bp = Blueprint("routes", __name__)
-
-# Register individual route modules
-routes_bp.register_blueprint(user_routes)
