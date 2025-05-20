7f45da071f0b7ccfea93a721cb2d1c81ba41fa36
LAPTOP-RN4FBF9J\lhowa <lhowat624@hotmail.com>
2025-05-20 17:41:30 +0100

    Getting username to come back from AUTH0

--- PersonalProject/src/project-ui/pages/LandingPage.tsx
+++ PersonalProject/src/project-ui/pages/LandingPage.tsx
@@ -1,30 +1,13 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { useEffect } from "react";
-import { upsertUser } from "../database/utils";
 
 function LandingPage() {
   const { user } = useAuth0();
-
-  useEffect(() => {
-    const createUser = async () => {
-      if (user && user.name !== undefined) {
-        const stored_user = localStorage.getItem("user_id");
-        if (!stored_user) {
-          const data = await upsertUser(user.name);
-          if (data && data[0]) {
-            localStorage.setItem("user_id", data[0].id);
-          }
-        }
-      }
-    };
-
-    createUser();
-  }, [])
+  const claimUrl = import.meta.env.VITE_AUTH0_CLAIM_URL;
 
   return (
     <div>
-    <p>Hello World</p>
-    <p>{user?.name}</p>
+      <p>Hello World</p>
+      <p>{user?.[`${claimUrl}/username`]}</p>
     </div>
   );
 };
