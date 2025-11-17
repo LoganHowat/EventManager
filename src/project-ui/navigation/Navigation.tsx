e78012fa0d6df071024c495745a2246476da9dc7
LAPTOP-RN4FBF9J\lhowa <lhowat624@hotmail.com>
2025-11-17 18:29:56 +0000

    Hiding navigation when users are not logged in

--- src/project-ui/navigation/Navigation.tsx
+++ src/project-ui/navigation/Navigation.tsx
@@ -2,7 +2,9 @@ import { Sidenav, Nav, Button } from 'rsuite';
 import { Menu } from '@rsuite/icons'
 import { Link } from 'react-router-dom';
 import { LogoutButton, Pages } from '..';
-import { useState } from 'react';
+import { useState } from 'react'
+import { useAuth0 } from "@auth0/auth0-react";
+;
 
 
 interface Props {
@@ -12,45 +14,48 @@ interface Props {
 function Navigation(props: Props) {
   const { pages } = props;
   const [expanded, setExpanded] = useState(false);
+  const { user } = useAuth0();
 
-  return (
-    <div className='sidenav-container'>
-      <Button
-        className='sidenav-menu-button'
-        onClick={() => setExpanded(!expanded)}
-      >
-        <Menu />
-      </Button>
-      <div className={expanded ? 'sidenav-open' : 'sidenav-closed'}>
-        <Sidenav
-          appearance='subtle'
+  if (user) {
+    return (
+      <div className='sidenav-container'>
+        <Button
+          className='sidenav-menu-button'
+          onClick={() => setExpanded(!expanded)}
         >
-          <Sidenav.Body>
-            <div className='sidenav-close-button'>
-              <Button 
-                className='sidenav-menu-button'
-                onClick={() => setExpanded(!expanded)}>
-                <Menu />
-              </Button>
-            </div>
-            <div className='sidenav-links'>
-              <Nav vertical>
-                {pages.map((page) => {
-                  return (
-                    <Nav.Item as={Link} to={page.path} key={page.name} onClick={() => setExpanded(!expanded)}>
-                      <h3>{page.name}</h3>
-                    </Nav.Item>)
-                })}
-              </Nav>
-            </div>
-            <div className='sidenav-logout'>
-              <LogoutButton />
-            </div>
-          </Sidenav.Body>
-        </Sidenav>
+          <Menu />
+        </Button>
+        <div className={expanded ? 'sidenav-open' : 'sidenav-closed'}>
+          <Sidenav
+            appearance='subtle'
+          >
+            <Sidenav.Body>
+              <div className='sidenav-close-button'>
+                <Button
+                  className='sidenav-menu-button'
+                  onClick={() => setExpanded(!expanded)}>
+                  <Menu />
+                </Button>
+              </div>
+              <div className='sidenav-links'>
+                <Nav vertical>
+                  {pages.map((page) => {
+                    return (
+                      <Nav.Item as={Link} to={page.path} key={page.name} onClick={() => setExpanded(!expanded)}>
+                        <h3>{page.name}</h3>
+                      </Nav.Item>)
+                  })}
+                </Nav>
+              </div>
+              <div className='sidenav-logout'>
+                <LogoutButton />
+              </div>
+            </Sidenav.Body>
+          </Sidenav>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Navigation;
